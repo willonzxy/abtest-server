36455b8263cdb94bf20baa6d9d96594694f2ebbb
江伟龙 <jiangweilong@aobi.com>
2021-03-25 19:05:44 +0800

    perf

--- app/router.js
+++ app/router.js
@@ -12,9 +12,9 @@ module.exports = app => {
   router.resources('layer',Auth + '/layer',controller.layer);
   router.resources('experiment',Auth + '/experiment',controller.experiment);
   router.resources('user',Auth + '/user',controller.user);
+  router.resources('launch',Auth + '/launch',controller.launch);
   router.post('/upload',controller.upload.create)
   router.post('/login',controller.user.login)
   router.get(Auth + '/logout',controller.user.logout)
   router.get(Auth + '/user_session',controller.user.getSession)
-  router.post(Auth + '/launch',controller.launch.launch);
 };
